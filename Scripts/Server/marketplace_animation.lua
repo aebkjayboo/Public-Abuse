4563d07fa4ce9cdb5bd4348cd1916e295c63bcd4
ProphecySkondo <zxcggfdrdg@gmail.com>
2026-08-02 10:16:55 -0400

    Added marketplace_animation.lua

---
+++ Scripts/Server/marketplace_animation.lua
@@ -0,0 +1,28 @@
+local Players = game:GetService("Players")
+
+---@param marketplaceid number
+---@return boolean
+local function PlayMarketplaceAnimation(marketplaceid)
+	local success, err = pcall(function()
+		local LocalPlayer = Players.LocalPlayer
+		local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
+		local Humanoid = Character:FindFirstChildOfClass("Humanoid")
+		local Animator = Humanoid:FindFirstChildOfClass("Animator")
+
+		local rbx = game:GetObjects("rbxassetid://" .. marketplaceid)
+		local animObject = rbx[1]
+
+		if animObject and animObject:IsA("Animation") then
+			local track = Animator:LoadAnimation(animObject)
+			track:Play()
+		else
+			warn("Not an Animation object:", animObject)
+		end
+	end)
+
+	return success
+end
+
+--PlayMarketplaceAnimation(130371895389423)
+
+return PlayMarketplaceAnimation
